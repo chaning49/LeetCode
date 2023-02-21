696c45120023b3fff25bb63f5a3475a9b90b5fe6
chaning49 <79316402+chaning49@users.noreply.github.com>
2023-02-21 11:28:42 +0900

    Time: 0 ms (100.00%), Space: 48.1 MB (72.85%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -5,8 +5,8 @@ class Solution {
         
         while (start < end) {
             int mid = (start + end) / 2;
-            if (mid % 2 == 1) mid--; // mid 값이 홀수라면 왼쪽 값으로 맞춰준다.
-            if (nums[mid] == nums[mid + 1]) start = mid + 2; // mid 값의 오른쪽이 같으면 한 쌍이 존재하는 값이므로 
+            if (mid % 2 == 1) mid--;
+            if (nums[mid] == nums[mid + 1]) start = mid + 2;
             else end = mid;
         }
         
@@ -14,14 +14,3 @@ class Solution {
     }
 }
 
-//         int ans = 0;
-//         if (nums.length == 1) return nums[0];
-        
-//         for (int i = 0; i < nums.length / 2; i++) {
-//             if (nums[2 * i] != nums[2 * i + 1]) {
-//                 ans = nums[2 * i];
-//                 return ans;
-//             }
-//         }
-//         if (nums.length % 2 == 1) ans = nums[nums.length - 1];
-//         return ans;
