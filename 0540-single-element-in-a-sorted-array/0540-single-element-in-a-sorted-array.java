0b156f43e3dca418952742e020e573b375b04af5
chaning49 <79316402+chaning49@users.noreply.github.com>
2023-02-21 11:05:08 +0900

    Time: 0 ms (100.00%), Space: 48.2 MB (60.82%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -1,15 +1,28 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-        int ans = 0;
-        if (nums.length == 1) return nums[0];
+        int start = 0;
+        int end = nums.length - 1;
+        int mid;
         
-        for (int i = 0; i < nums.length / 2; i++) {
-            if (nums[2 * i] != nums[2 * i + 1]) {
-                ans = nums[2 * i];
-                return ans;
-            }
+        while (start < end) {
+            mid = (start + end) / 2;
+            if (mid % 2 == 1) mid--;
+            if (nums[mid] == nums[mid + 1]) start = mid + 2;
+            else end = mid;
         }
-        if (nums.length % 2 == 1) ans = nums[nums.length - 1];
-        return ans;
+        
+        return nums[start];
     }
 }
+
+//         int ans = 0;
+//         if (nums.length == 1) return nums[0];
+        
+//         for (int i = 0; i < nums.length / 2; i++) {
+//             if (nums[2 * i] != nums[2 * i + 1]) {
+//                 ans = nums[2 * i];
+//                 return ans;
+//             }
+//         }
+//         if (nums.length % 2 == 1) ans = nums[nums.length - 1];
+//         return ans;
