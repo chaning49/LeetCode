4932c0859bab71dd02adbe6dea0a7c4c9ebcfa75
chaning49 <79316402+chaning49@users.noreply.github.com>
2023-02-24 00:09:22 +0900

    Time: 134 ms (25.62%), Space: 86.4 MB (13.09%) - LeetHub

--- 0502-ipo/0502-ipo.java
+++ 0502-ipo/0502-ipo.java
@@ -2,8 +2,8 @@ import java.util.PriorityQueue;
 
 class Solution {
     public int findMaximizedCapital(int k, int w, int[] profits, int[] capital) {
-        PriorityQueue<int[]> minHeap = new PriorityQueue<>((a, b) -> (a[0] - b[0]));
-        PriorityQueue<int[]> maxHeap = new PriorityQueue<>((a, b) -> (b[1] - a[1]));
+        PriorityQueue<int[]> minHeap = new PriorityQueue<>((a, b) -> (a[0] - b[0])); // 2차원 배열에서 앞에 위치한 값: capital
+        PriorityQueue<int[]> maxHeap = new PriorityQueue<>((a, b) -> (b[1] - a[1])); // 2차원 배열에서 뒤에 위치한 값: profits
         
         for (int i = 0; i < capital.length; i++)
             minHeap.add(new int[] {capital[i], profits[i]});
