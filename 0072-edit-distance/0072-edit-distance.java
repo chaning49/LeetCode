b420a17f67caec6dea49ef05a383ff3a909de8ec
chaning49 <79316402+chaning49@users.noreply.github.com>
2023-02-27 00:01:43 +0900

    Time: 6 ms (67.73%), Space: 42.8 MB (25.18%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -5,9 +5,9 @@ class Solution {
         
         int[][] dp = new int[m + 1][n + 1];
         
-        for (int i = 1; i <= m; i++)
+        for (int i = 0; i <= m; i++)
             dp[i][0] = i;
-        for (int j = 1; j <= n; j++)
+        for (int j = 0; j <= n; j++)
             dp[0][j] = j;
         
         for (int i = 1; i <= m; i++) {
