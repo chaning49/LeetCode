e2b70705a360a2d43796484c3f13a5e9a8f75558
chaning49 <79316402+chaning49@users.noreply.github.com>
2023-03-30 02:50:29 +0900

    Time: 13 ms (25.77%), Space: 42.1 MB (32.47%) - LeetHub

---
+++ 1402-reducing-dishes/1402-reducing-dishes.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int maxSatisfaction(int[] satisfaction) {
+        int ans = 0;
+        Arrays.sort(satisfaction);
+        
+        List<Integer> satList = new ArrayList<>(satisfaction.length);
+        for (int s : satisfaction)
+            satList.add(s);
+        
+        List<Integer> index = new ArrayList<>(satisfaction.length);
+        for (int i = 1; i <= satisfaction.length; i++)
+            index.add(i);
+        
+        while (satList.size() > 0) {
+            int temp = 0;
+            for (int i = 0; i < satList.size(); i++)
+                temp += satList.get(i) * index.get(i);
+            ans = Math.max(ans, temp);
+            temp = 0;
+            satList.remove(0);
+            index.remove(index.size() - 1);
+        }
+        
+        return ans;
+    }
+}
