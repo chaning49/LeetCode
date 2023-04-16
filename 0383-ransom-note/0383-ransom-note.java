36c44f656a0aa82adaac2aeb0119b83c4cd8a55d
chaning49 <79316402+chaning49@users.noreply.github.com>
2023-04-17 00:58:21 +0900

    Time: 2 ms (98.43%), Space: 43.3 MB (25.60%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int[] alpha = new int[26];
+        for (char c : magazine.toCharArray())
+            ++alpha[c - 'a'];
+        
+        for (char c : ransomNote.toCharArray()) {
+            if (alpha[c - 'a'] == 0)
+                return false;
+            --alpha[c - 'a'];
+        }
+        
+        return true;
+    }
+}
