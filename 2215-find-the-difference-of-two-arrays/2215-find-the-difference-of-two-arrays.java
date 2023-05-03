5e38400bacd4492efdfd49a67f0ca6cbeb6aa06b
chaning49 <79316402+chaning49@users.noreply.github.com>
2023-05-03 21:04:00 +0900

    Time: 11 ms (86.46%), Space: 43.2 MB (77.18%) - LeetHub

--- 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.java
+++ 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.java
@@ -1,33 +1,30 @@
 class Solution {
     public List<List<Integer>> findDifference(int[] nums1, int[] nums2) {
-        List<List<Integer>> answer = new ArrayList<>(2);
-        answer.add(new ArrayList<>());
-        answer.add(new ArrayList<>());
+        List<List<Integer>> answer = new ArrayList<>();
+        List<Integer> list1 = new ArrayList<>();
+        List<Integer> list2 = new ArrayList<>();
+        Set<Integer> set1 = new HashSet<>();
+        Set<Integer> set2 = new HashSet<>();
         
-        // 배열에 포함된 중복 제거하면서 List 생성
-        List<Integer> list1 = addList(nums1);
-        List<Integer> list2 = addList(nums2);
+        for (int n : nums1)
+            set1.add(n);
+        for (int n : nums2)
+            set2.add(n);
         
         // 리스트에서 서로 존재하는 값을 제거
-        for (Integer l1 : list1) {
-            if (!list2.contains(l1))
-                answer.get(0).add(l1);
+        for (Integer l1 : set1) {
+            if (!set2.contains(l1))
+                list1.add(l1);
         }
         
-        for (Integer l2 : list2) {
-            if (!list1.contains(l2))
-                answer.get(1).add(l2);
+        for (Integer l2 : set2) {
+            if (!set1.contains(l2))
+                list2.add(l2);
         }
+        
+        answer.add(list1);
+        answer.add(list2);
         
         return answer;
-    }
-    
-    private List<Integer> addList(int[] arr) {
-        List<Integer> list = new ArrayList<>(arr.length);
-        for (int i = 0; i < arr.length; i++) {
-            if (!list.contains(arr[i]))
-                list.add(arr[i]);
-        }
-        return list;
     }
 }
