c5fa14f2c15a4c4b12cb8c6ee816a98130129007
chaning49 <79316402+chaning49@users.noreply.github.com>
2023-05-17 10:56:01 +0900

    Time: 11 ms (61.16%), Space: 41.1 MB (99.13%) - LeetHub

--- 2466-count-ways-to-build-good-strings/2466-count-ways-to-build-good-strings.java
+++ 2466-count-ways-to-build-good-strings/2466-count-ways-to-build-good-strings.java
@@ -3,6 +3,7 @@ class Solution {
         int[] dp = new int[high + 1];
         dp[0] = 1;
         int md = 1000000007;
+        
         for (int i = Math.min(zero, one); i <= high; i++) {
             if (i >= zero) {
                 dp[i] = (dp[i] + dp[i - zero]) % md;
@@ -11,10 +12,12 @@ class Solution {
                 dp[i] = (dp[i] + dp[i - one]) % md;
             }
         }
+        
         int sum = 0;
         for (int i = low; i <= high; i++) {
             sum = (sum + dp[i]) % md;
         }
+        
         return sum;
     }
 }
