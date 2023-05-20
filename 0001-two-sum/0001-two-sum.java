eca0c02dd9074f2b78c7d0781dc63690378ba70e
chaning49 <79316402+chaning49@users.noreply.github.com>
2023-05-21 04:16:14 +0900

    Time: 2 ms (80.41%), Space: 44 MB (5.96%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,10 +1,15 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> findIndex = new HashMap<>();
+        
         for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] + nums[j] == target)
-                    return new int[] {i, j};
+            // target을 찾은 경우
+            if (findIndex.containsKey(target - nums[i])) {
+                return new int[] {findIndex.get(target - nums[i]), i};
             }
+            
+            // 아닌 경우 - 값에 인덱스를 매핑
+            findIndex.put(nums[i], i);
         }
         return new int[] {};
     }
